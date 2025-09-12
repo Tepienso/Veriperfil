85ee54028467ca86e627f2111da16a74852f53f3
Juan Pablo <j_vera_candioti@hotmail.com>
2025-09-12 18:26:44 -0300

    Mover next.config a JS y desactivar lightningcss

---
+++ veriperfil-next/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    css: {
+      lightningcss: false
+    }
+  }
+}
+
+module.exports = nextConfig
